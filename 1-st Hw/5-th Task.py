a1c6db8aa2cf65b2df6dc99aefe36747009b9c1c
Ch1kez <104025069+Ch1kez@users.noreply.github.com>
2023-02-20 00:11:57 +0300

    Add files via upload

---
+++ 1-st Hw/5-th Task.py
@@ -0,0 +1,8 @@
+def uefa_euro_2016(teams, scores):
+    if scores[0] == scores[1]:
+        return f'At match {teams[0]} - {teams[1]}, teams played draw.'
+    else:
+        if scores[0] > scores[1]:
+            return f'At match {teams[0]} - {teams[1]}, {teams[0]} won!'
+        else:
+            return f'At match {teams[0]} - {teams[1]}, {teams[1]} won!'
