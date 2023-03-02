63c0ba185f7ce2f61ea898cb5b1bfbd8f7384315
Ch1kez <104025069+Ch1kez@users.noreply.github.com>
2023-03-03 02:05:14 +0300

    Update 8-th Task.py

--- 2-nd Hw/8-th Task.py
+++ 2-nd Hw/8-th Task.py
@@ -2,8 +2,5 @@
 '''
     Выполнил:   Курнаев Данила
     Группа:     М7О-406с-19
-    link = https://www.codewars.com//kata/5803a6d8db07c59fff00015f
+    link = 
 '''
-
-def starts_with(st, prefix):
-    return st.startswith(prefix)
