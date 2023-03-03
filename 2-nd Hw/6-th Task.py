938d6e6f1e1679f7b1fe2ddd8353d57024625c1a
Ch1kez <104025069+Ch1kez@users.noreply.github.com>
2023-03-03 23:11:00 +0300

    Update 6-th Task.py

--- 2-nd Hw/6-th Task.py
+++ 2-nd Hw/6-th Task.py
@@ -9,21 +9,26 @@
 def is_very_even_number(n):
     summa = 0
 
-    for el in str(n):
-        summa += int(el)
-
-    if len(str(summa)) == 1:
-        if summa % 2 == 0:
+    if n <= 9:
+        if n % 2 == 0:
             return True
         else:
             return False
+        
     else:
-        while len(str(summa)) > 1:
+        
+        for el in str(n):
+            summa += int(el)
+            
+        while summa > 9:
+            rez = 0
             for el in str(summa):
-                summa += int(el)
+                print(el)
+                rez += int(el)
+            summa = rez
+        
         if summa % 2 == 0:
             return True
         else:
             return False
-
-is_very_even_number(88)
+       
