f190bd4f2574716018ad70cc854ad65cd2aed9f7
Ch1kez <104025069+Ch1kez@users.noreply.github.com>
2023-03-03 22:05:50 +0300

    Update 3-d Task.py

--- 2-nd Hw/3-d Task.py
+++ 2-nd Hw/3-d Task.py
@@ -6,16 +6,31 @@
 '''
 
 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
         head_list = []
+        
         while head:
             head_list.append(head.val)
             head = head.next
-        mid = len(head_list) % 2
-        if mid == 0:
-            head_list = head_list[mid:]
-            return head_list
-        else:
-            head_list = head_list[mid + 1:]
-            return head_list
+
+        mid = len(head_list) // 2
+        mod = len(head_list) % 2
+    
+        head_list = head_list[mid:]
+
+        head_list_node = ListNode(head_list[0])
+        current = head_list_node
+        head_list = head_list[1:]
+
+        for el in head_list:
+            current.next = ListNode(el)
+            current = current.next
+
+        return head_list_node
+    
