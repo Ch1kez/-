978cbaa29369059e765115a3efd194648b6b6b67
Ch1kez <104025069+Ch1kez@users.noreply.github.com>
2023-03-04 22:43:54 +0300

    Update 4-th Task.py

--- 2-nd Hw/4-th Task.py
+++ 2-nd Hw/4-th Task.py
@@ -2,6 +2,33 @@
 '''
     Выполнил:   Курнаев Данила
     Группа:     М7О-406с-19
-    link =
+    link = https://leetcode.com/problems/remove-duplicates-from-sorted-list/submissions/909100641/
 '''
 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return head
+        
+        head_list = [head.val]
+
+        while head.next:
+            if not head.val == head.next.val:
+                head_list.append(head.next.val)
+
+            head = head.next
+
+        head_list_node = ListNode(head_list[0])
+        current = head_list_node
+
+        for el in head_list[1:]:
+            current.next = ListNode(el)
+            current = current.next
+
+        return head_list_node
