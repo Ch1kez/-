5b3075d6cf8fa21502a3f853e91c4e3dc456ac1d
Ch1kez <104025069+Ch1kez@users.noreply.github.com>
2023-03-04 20:53:25 +0300

    Update 5-th Task.py

--- 2-nd Hw/5-th Task.py
+++ 2-nd Hw/5-th Task.py
@@ -2,6 +2,13 @@
 '''
     Выполнил:   Курнаев Данила
     Группа:     М7О-406с-19
-    link =
+    link = codewars.com/kata/5a1ee4dfffe75f0fcb000145/train/python
 '''
 
+def bingo(array): 
+    ar = [2, 9, 14, 7, 15]
+    print(ar,'\n',array)
+    for el in ar:
+        if el not in array:
+            return 'LOSE'
+    return 'WIN'
